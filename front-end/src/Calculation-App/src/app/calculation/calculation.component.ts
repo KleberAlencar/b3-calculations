fb140f949c6ca06b44edd41d913d981b69cbe0a9
KLEBER_ALENCAR <kleber.alencar@outlook.com>
2023-02-13 15:42:08 -0300

    Refactoring code

--- front-end/src/Calculation-App/src/app/calculation/calculation.component.ts
+++ front-end/src/Calculation-App/src/app/calculation/calculation.component.ts
@@ -8,17 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculationComponent implements OnInit {
 
-  public cdbCalculation: any;
+  public cdbCalculation: any = [];
+  public taxDiscounts: any = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getCdbCalculation();
+    this.searchTaxDiscounts();
   }
 
   public getCdbCalculation(): void {
-    this.http.get('http://localhost:5000/api/calculations/cdb?Investiment=1000&MonthsQuantity=6').subscribe(
+    this.http.get('http://localhost:5000/api/calculations/cdb?Investiment=1000&MonthsQuantity=10').subscribe(
       response => this.cdbCalculation = response,
+      error => console.log(error)
+    );
+  }
+
+  public searchTaxDiscounts(): void {
+    this.http.get('http://localhost:5000/api/calculations/tax-discounts').subscribe(
+      response => this.taxDiscounts = response,
       error => console.log(error)
     );
   }
